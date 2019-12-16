ffc9a5cb07c5480e624c25103894203e394d48fe
Ben Woodley <ben@benw.co.uk>
2019-12-16 15:29:33 +0000

    ehh

--- bpy_mesh_reduce.py
+++ bpy_mesh_reduce.py
@@ -209,6 +209,8 @@ def main():
                         / local_texture_path.parent.name
                         / local_texture_path.name
                     )
+                    print("texture path: " + str(local_texture_path))
+                    print("(" + str(texture_path) + ")")
                     input_mesh_list.append(Mesh(obj_path, texture_path))
                     break
     for input_mesh in input_mesh_list:
